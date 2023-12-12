e24c474b7e62b72663987dc27b5be6edd63472c4
sverre <sverrwie@stud.ntnu.no>
2023-12-12 10:13:37 +0100

    Design improvements

--- sverre_experiment_test_claims/client/src/examples/Advertise.jsx
+++ sverre_experiment_test_claims/client/src/examples/Advertise.jsx
@@ -29,14 +29,12 @@ import {
       //player.stage.submit();
     }
 
-
     function handleProductionChoice(e, productionQuality, cost) {
       player.round.set("productionQuality",productionQuality);
       if (player.round.get("productionQuality") === "low"){player.round.set("productionCost", 5)}
       if (player.round.get("productionQuality") === "high"){player.round.set("productionCost", 9)}     
       console.log("Saved production quality to player.round object: ", productionQuality);
       console.log("Saved production cost to player.round object: ", player.round.get("productionCost"));
-
     }
     
     function handleAdverisementChoice(e, advertisementQuality) {
@@ -83,20 +81,27 @@ import {
     return (
       <div className="md:min-w-96 lg:min-w-128 xl:min-w-192 flex flex-col items-center space-y-10">
         {}
-        <br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <div>
         <h1><b>You are a producer of toothpaste.</b> </h1>
-        <p>You will now decide what to produce, how to advertise it and the price.</p>
+        <h1>You will now decide what to produce, how to advertise it and the price.</h1>
+
+        </div>
+
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <h1><b>You are a producer of toothpaste</b></h1>
-          <h1><b>Choose what to produce.</b> All your products this round will be the quality you select.</h1>
+          <h1><b>Choose what to produce.</b> All your products this round will be the quality you select. <br/> Your current choice is to produce: <b>{player.round.get("productionQuality")} </b> quality toothspaste.</h1>
           <div className="flex justify-center space-x-4"> {/* This flex container will lay out its children (products) in a row */}
           <ProductionAlternative title="Standard Toothpaste" cost="5" quality="low" imageUrl={"url(/images/toothpastestandard.jpg)"} on_button_click={(e) => handleProductionChoice(e, "low")}/>
           <ProductionAlternative title="Amazing Toothpaste" cost="9" quality="high" imageUrl={"url(/images/toothpaseamazing.jpg)"} on_button_click={(e) => handleProductionChoice(e, "high")}/> {/*Here we need to pass what kind of advertisement option the player chose*/ }
         </div>
         <br/><br/><br/><br/><br/><br/><br/>
-          <h1><b>Choose how you want to advertise it.</b> All your products will be advertised this way</h1>
+          <h1><b>Choose how you want to advertise it.</b> All your products will be advertised this way.</h1>
+          <p>When people are buying, they will only know the price and the advertised quality.
+            They will not know the true quality until they have bought the product.</p> <br/>
           <p><strong>Note: </strong>You have the ability to make any kind of advertisement<br/> about your product in order to maximize your sales.</p>
+          <p>Your current choice is to advertise your product as: <b>{player.round.get("advertisementQuality")} </b> quality toothspaste.</p>
           <div className="flex justify-center space-x-4"> {/* This flex container will lay out its children (products) in a row */}
           <AdvertisementAlternative title="Standard Toothpaste (low quality)"  quality="low" imageUrl={"url(/images/toothpastestandard.jpg)"} on_button_click={(e) => handleAdverisementChoice(e, "low")}/>
           <AdvertisementAlternative title="Amazing Toothpaste (high quality)"  quality="high" imageUrl={"url(/images/toothpaseamazing.jpg)"} on_button_click={(e) => handleAdverisementChoice(e, "high")}/>
@@ -107,6 +112,8 @@ import {
           <p> A typical price for <b>low </b> quality toothpaste is : $10 </p>
           <p> A typical price for <b>high</b> quality toothpaste is : $15 </p>
           <p><strong>Note: </strong>You have the ability to set any kind of price<br/> for your product in order to maximize your sales.</p>
+
+          <p>Your current choice is to sell at a price of: <b>{player.round.get("priceOfProduct")} </b></p>
           <div className="flex justify-center space-x-4"> 
           <PriceButton text={'$10'} on_button_click={(e) => handlePriceChoice(e, 10)}></PriceButton>
           <PriceButton text={'$15'} on_button_click={(e) => handlePriceChoice(e, 15)}></PriceButton>
@@ -212,9 +219,9 @@ import {
     let profit = producerPlayer.round.get("priceOfProduct") - producerPlayer.round.get("productionCost")
     return(
       <div>
-        <p>When people are buying, they will only know the price and the advertised quality. They will not know the true quality until they have bought the product.</p> <br/>
+        
         <p>You have chosen to produce <b>{producerPlayer.round.get("productionQuality")}</b> quality toothpaste and advertise it as <b>{producerPlayer.round.get("advertisementQuality")}</b> quality toothpase at a <b>price of ${producerPlayer.round.get("priceOfProduct")}</b></p>
-        <p>This gives a profit of:  ${profit} per product sold.</p>
+        <h1><p>This gives a <b>profit of  ${profit}</b> per product sold.</p></h1>
 
       </div>
     )
